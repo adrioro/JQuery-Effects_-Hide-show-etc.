ff8b96ecacf131f948deb3de3a93787dae5911dc
adrioro <artistsvillage@me.com>
2019-01-28 15:45:25 -0500

    Third commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*
 $("#myButton").click(function () {
     $(".myDiv1").hide("slow");
 
@@ -7,11 +6,11 @@ $("textarea").keypress(function (a) {
     var keycode = a.keyCode ? a.keyCode : a.which;
 
     if (keycode == "13") {
-        $(".myDiv1").hide("slow");
+        $(".myDiv2").hide("slow");
     }
 
 });
-*/
+
 
 $("#myButton2").click(function () {
     $(".myDiv3").toggle();
