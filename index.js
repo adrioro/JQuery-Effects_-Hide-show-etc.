40749a28c0ba6785e0d0ad55039f4869fd1d6446
adrioro <artistsvillage@me.com>
2019-01-28 15:43:27 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-$("#myButton").onclick(function () {
+/*
+$("#myButton").click(function () {
     $(".myDiv1").hide("slow");
+
+});
+$("textarea").keypress(function (a) {
+    var keycode = a.keyCode ? a.keyCode : a.which;
+
+    if (keycode == "13") {
+        $(".myDiv1").hide("slow");
+    }
+
+});
+*/
+
+$("#myButton2").click(function () {
+    $(".myDiv3").toggle();
 
 });
